46ee1e3874e45b5a3fc30d4c5de1e70234d9470d
Danila Ustalov <003daniel@mail.ru>
2023-11-21 00:28:31 +0300

    removed unnecessary code

--- src/main/java/com/example/sanatorium/controllers/ResidentialApartmentBookingController.java
+++ src/main/java/com/example/sanatorium/controllers/ResidentialApartmentBookingController.java
@@ -1,18 +1,14 @@
 package com.example.sanatorium.controllers;
 
 import com.example.sanatorium.models.ResidentialApartmentBooking;
-import com.example.sanatorium.repos.ClientRepo;
 import com.example.sanatorium.services.ResidentialApartmentBookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/apartment-bookings")
@@ -28,12 +24,6 @@ public class ResidentialApartmentBookingController {
 
     @PostMapping("/create")
     public String createApartmentBooking(ResidentialApartmentBooking apartmentBooking) {
-//        @RequestParam long apartmentBookingId,
-//        @RequestParam long apartmentId,
-//        @RequestParam long clientId,
-//        @RequestParam LocalDateTime arrivalDateTime,
-//        @RequestParam LocalDateTime departureDateTime
-        //apartmentBookingService.saveApartmentBooking(apartmentBookingId, apartmentId, clientId, arrivalDateTime, departureDateTime);
         apartmentBookingService.saveApartmentBooking(apartmentBooking);
         return "redirect:/apartment-bookings";
     }
@@ -50,15 +40,6 @@ public class ResidentialApartmentBookingController {
         return "apartmentsBooking/apartment-booking-info";
     }
 
-//    @PostMapping("/put")
-//    public String changeApartmentBooking(@RequestParam long apartmentBookingId,
-//                                         @RequestParam long apartmentId,
-//                                         @RequestParam long clientId,
-//                                         @RequestParam LocalDateTime arrivalDateTime,
-//                                         @RequestParam LocalDateTime departureDateTime) {
-//        apartmentBookingService.updateApartmentBooking(apartmentBookingId, apartmentId, clientId, arrivalDateTime, departureDateTime);
-//        return "redirect:/apartment-bookings/" + apartmentBookingId;
-//    }
 
     @PostMapping("/put")
     public String changeApartmentBooking(ResidentialApartmentBooking apartmentBooking) {
--- src/main/java/com/example/sanatorium/services/ResidentialApartmentBookingService.java
+++ src/main/java/com/example/sanatorium/services/ResidentialApartmentBookingService.java
@@ -22,21 +22,6 @@ public class ResidentialApartmentBookingService {
         return apartmentBookingRepo.findAll();
     }
 
-    public void saveApartmentBooking(long apartmentBookingId,
-                                     long apartmentId,
-                                     long clientId,
-                                     LocalDateTime arrivalDateTime,
-                                     LocalDateTime departureDateTime) {
-
-        apartmentBookingRepo.save(new ResidentialApartmentBooking(
-                apartmentBookingId,
-                apartmentService.getResidentialApartmentById(apartmentId),
-                clientService.getClientById(clientId),
-                arrivalDateTime,
-                departureDateTime
-        ));
-    }
-
     public void saveApartmentBooking(ResidentialApartmentBooking apartmentBooking) {
 
         apartmentBookingRepo.save(apartmentBooking);
@@ -50,25 +35,6 @@ public class ResidentialApartmentBookingService {
         return apartmentBookingRepo.findById(id).orElse(null);
     }
 
-    public void updateApartmentBooking(long apartmentBookingId,
-                                       long apartmentId,
-                                       long clientId,
-                                       LocalDateTime arrivalDateTime,
-                                       LocalDateTime departureDateTime) {
-        Client client = clientService.getClientById(clientId);
-        ResidentialApartment apartment = apartmentService.getResidentialApartmentById(apartmentId);
-        ResidentialApartmentBooking apartmentBooking = new ResidentialApartmentBooking(
-                apartmentBookingId,
-                apartmentService.getResidentialApartmentById(apartmentId),
-                clientService.getClientById(clientId),
-                arrivalDateTime,
-                departureDateTime);
-        client.getResidentialApartmentBookings().add(apartmentBooking);
-        clientService.saveClient(client);
-        apartment.getResidentialApartmentBookings().add(apartmentBooking);
-        apartmentService.saveApartment(apartment);
-        apartmentBookingRepo.save(apartmentBooking);
-    }
 
     public void updateApartmentBooking(ResidentialApartmentBooking apartmentBooking) {
         apartmentBookingRepo.save(apartmentBooking);
